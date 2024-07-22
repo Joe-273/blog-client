903dc765562eb7e5e873992daf2dc2115745e4a7
QihaoWei <QihaoWei.273@Outlook.com>
2024-07-22 20:33:53 +0800

    完成Avatar组件

---
+++ src/components/Avatar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <img
+      :src="imgUrl"
+      :style="{
+        width: imgSize + 'px',
+        height: imgSize + 'px',
+      }"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imgUrl: {
+      type: String,
+      required: true,
+    },
+    imgSize: {
+      type: Number,
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+img {
+  object-fit: cover;
+  border-radius: 50%;
+  transition: 0.25s ease;
+  &:hover {
+    transform: scale(1.1);
+    box-shadow: 0 0 25px rgba(0, 0, 0, 0.5);
+  }
+}
+</style>
