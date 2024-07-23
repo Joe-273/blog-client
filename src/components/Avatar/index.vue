c26e3abbba0c76a4440c12d7a445816c9cba4f49
QihaoWei <QihaoWei.273@Outlook.com>
2024-07-23 08:22:32 +0800

    优化工程结构，添加了组件文档，修改了配置文件（脚手架版本等等）

---
+++ src/components/Avatar/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <img
+    class="avatar-container"
+    :src="imgUrl"
+    :style="{
+      width: imgSize + 'px',
+      height: imgSize + 'px',
+    }"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    imgUrl: {
+      type: String,
+      required: true,
+    },
+    imgSize: {
+      type: Number,
+      default: 150,
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.avatar-container {
+  object-fit: cover;
+  border-radius: 50%;
+  transition: 0.25s ease;
+  &:hover {
+    transform: scale(1.1);
+    box-shadow: 0 0 25px rgba(0, 0, 0, 0.5);
+  }
+}
+</style>
---
+++ src/components/Pager/preview.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Pager :total="total" :current="current" @pageChange="handlePageChange" />
+  </div>
+</template>
+
+<script>
+import Pager from "./";
+export default {
+  components: {
+    Pager,
+  },
+  data() {
+    return {
+      current: 1,
+      total: 302,
+    };
+  },
+  methods: {
+    handlePageChange(newPage) {
+      this.current = newPage;
+      console.log("加载当前页数据");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
